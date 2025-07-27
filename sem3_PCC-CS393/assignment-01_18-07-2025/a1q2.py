8e62dadd2e4e037d6e16cd5dc5e174fb8898fa88
Dipam Pandit <dipampandit21@gmail.com>
2025-07-27 19:52:52 +0530

    Delete sem3_PCC-CS393/assignment-01_18-07-2025 directory

--- sem3_PCC-CS393/assignment-01_18-07-2025/a1q2.py
+++
@@ -1,5 +0,0 @@
-num_int = 42
-num_float = 3.14159
-
-print(f"Integer as string: {str(num_int)}")
-print(f"Float as string: {str(num_float)}")
--- sem3_PCC-CS393/assignment-01_18-07-2025/a1q4.py
+++
@@ -1,4 +0,0 @@
-values = [42, 3.14, "Hello", True, 5+6j, [1,2,3], {"key":"value"}]
-
-for v in values:
-    print(f"Value: {v} | Type: {type(v)}")
